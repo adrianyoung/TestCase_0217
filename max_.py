12885db585ac0e6925342616d42e429d33fd91ab
adrianyoung <yd0301@outlook.com>
2019-02-17 14:39:31 +0800

    Update

---
+++ max_.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+def max_(*args, **kwargs):
+
+    # 无参数报错
+    if len(args) == 0:
+        raise TypeError("max_ expected 1 arguments, got 0")
+
+    # 无效参数报错
+    for key in kwargs.keys():
+        if key not in ('key', 'default'):
+            raise TypeError("'{}' is an invalid keyword argument for this function".format(key))
+
+    # 参数数量为一
+    if len(args) == 1:
+        src = args[0]
+        # 对象不可迭代
+        if not hasattr(src, '__iter__'):
+            raise TypeError("'{}' object is not iterable".format(str(type(src)).split("'")[1]))
+        # 对象为空
+        if len(src) == 0:
+            if 'default' in kwargs.keys():
+                return kwargs['default']
+            else:
+                raise ValueError("max_() arg is an empty sequence")
+        # 执行前置处理
+        if 'key' in kwargs.keys():
+            func = kwargs['key']
+            if hasattr(kwargs['key'], '__call__'):
+                if isinstance(src, dict):
+                    obj = { e:func(e) for e in src.keys() }
+                    idx = dic_check(obj)
+                    return idx
+                else:
+                    obj = [ func(e) for e in src ]
+                    idx = iter_check(obj)
+                    return src[idx]
+            else:
+                raise TypeError("'{}' object is not callable".format(str(type(func)).split("'")[1]))
+        # 不执行前置处理
+        else:
+            if isinstance(src, dict):
+                obj = src.keys()
+                idx = key_check(obj)
+                return idx
+            else:
+                idx = iter_check(src)
+                return src[idx]
+
+    # 参数数量多于一
+    if len(args) >= 2:
+        src = args
+        # 不可设置默认最大值
+        if 'default' in kwargs.keys():
+            raise TypeError("Cannot specify a default for max_() with multiple positional arguments")
+        # 执行前置处理
+        if 'key' in kwargs.keys():
+            func = kwargs['key']
+            if hasattr(kwargs['key'], '__call__'):
+                obj = [ func(e) for e in src ]
+                idx = iter_check(obj)
+                return src[idx]
+            else:
+                raise TypeError("'{}' object is not callable".format(str(type(func)).split("'")[1]))
+        # 不执行前置处理
+        else:
+            idx = iter_check(src)
+            return src[idx]
+
+# 字典查询处理
+def dic_check(obj):
+    for cnt, key in enumerate(obj.keys()):
+        if cnt == 0:
+            val = obj[key]
+            idx = key
+        else:
+            if obj[key] > val:
+                idx = key
+    return idx
+
+# 键值查询处理
+def key_check(obj):
+    for cnt, key in enumerate(obj):
+        if cnt == 0:
+            val = key
+        else:
+            if key > val:
+                val = key
+    return val
+
+# 字符串查询处理
+def iter_check(obj):
+    idx = 0
+    for pos in range(len(obj)-1):
+        if obj[pos+1] > obj[pos]:
+            idx = pos+1
+    return idx
+
+if __name__ == "__main__":
+
+    max_(1)
+    max_(1,5,8,7,-9, key=abs)
+
+    # max()
+    # max(key=abs)
+    # max(default='a')
+
+    # max(1)
+    # max(1, default='a')
+    # max(1, key=lambda x:-x)
+    # max(1, key=1)
+
+    # max(())
+    # max('')
+
+    # max(1,2,3,default='aaa')
+
+    # max(1,-5, abc=abs)
+
+    # max([1,2,'3'], default='a')
+    # max(1,2,'3')
+    # max([1,2], (1,4))
---
+++ test_case.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from no_name_ import no_name_
+from max_ import max_
+
+class TestFunc(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_no_name_(self):
+
+        # 空字符串
+        self.assertTrue(no_name_('',''))
+        self.assertFalse(no_name_('','abbc'))
+        # 同构乱序
+        self.assertTrue(no_name_('cabb','abbc'))
+        # 异构长度相等
+        self.assertFalse(no_name_('cabd','abbc'))
+        # 长度不等
+        self.assertFalse(no_name_('cabbdd','abbc'))
+
+    def test_max_(self):
+
+        # 默认
+        self.assertEqual(max([], default=0), max_([], default=0))
+        # 多参数
+        self.assertEqual(max(1, 3, -9), max_(1, 3, -9))
+        # 支持浮点
+        self.assertEqual(max(1.0, 3, -9), max_(1.0, 3, -9))
+        # 单参数可迭代对象
+        self.assertEqual(max((1, 3, -9)), max_((1, 3, -9)))
+        self.assertEqual(max([1, 3, -9]), max_([1, 3, -9]))
+        self.assertEqual(max([1, 3, -9], [1, 2, -4]), max_([1, 3, -9], [1, 2, -4]))
+        self.assertEqual(max({'a':1, 'b':3, 'c':-9}), max_({'a':1, 'b':3, 'c':-9}))
+        # 前置处理
+        self.assertEqual(max(1, 3, -9, key=abs), max_(1, 3, -9, key=abs))
+        dic = {'a':1, 'b':3, 'c':-9}
+        self.assertEqual(max(dic, key=dic.get), max_(dic, key=dic.get))
+        # 字符比较
+        self.assertEqual(max('139'), max_('139'))
+        self.assertEqual(max('abc'), max_('abc'))
+        # 稳定性
+        a =  ['a']
+        b1 = ['a', 'b']
+        b2 = ['a', 'b']
+        self.assertNotEqual(id(b1), id(b2))
+        self.assertEqual(id(max(a, b1, b2)), id(b1))
+        self.assertEqual(id(max_(a, b1, b2)), id(b1))
+        # 捕捉异常
+        # 无参数
+        self.assertRaisesRegexp(TypeError,"expected 1 arguments, got 0",max_)
+        # 无效参数
+        self.assertRaisesRegexp(TypeError,"is an invalid keyword argument for this function", max_, 1, 3, -9, keys=abs)
+        # 单参数对象为空
+        self.assertRaisesRegexp(ValueError,"arg is an empty sequence", max_, [])
+        # 单参数对象不可迭代
+        self.assertRaisesRegexp(TypeError,"object is not iterable", max_, 1)
+        # 前置方法无效
+        self.assertRaisesRegexp(TypeError,"object is not callable", max_, 1, 3, -9, key=1)
+        # 多参数不可设置默认
+        self.assertRaisesRegexp(TypeError,"Cannot specify a default for .+ with multiple positional arguments", max_, 1, 3, -9, default=0)
+
+if __name__ == "__main__":
+
+    suite = unittest.TestSuite()
+
+    suite.addTest(TestFunc("test_no_name_"))
+    suite.addTest(TestFunc("test_max_"))
+
+    #runner = unittest.TextTestRunner(verbosity=2)
+    #runner.run(suite)
+
+    with open('test_report.txt', 'w') as fp:
+        runner = unittest.TextTestRunner(stream=fp, verbosity=2)
+        runner.run(suite)
+
+
